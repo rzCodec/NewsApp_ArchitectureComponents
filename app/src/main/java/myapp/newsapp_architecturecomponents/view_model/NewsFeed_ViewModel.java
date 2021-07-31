e73d4a231802063d0be297eba4d86267987eebb6
Ronald <laironald52@gmail.com>
2021-07-31 20:41:59 +0200

    Update NewsFeed_ViewModel.java

--- app/src/main/java/myapp/newsapp_architecturecomponents/view_model/NewsFeed_ViewModel.java
+++ app/src/main/java/myapp/newsapp_architecturecomponents/view_model/NewsFeed_ViewModel.java
@@ -14,50 +14,11 @@ public class NewsFeed_ViewModel extends ViewModel {
     private Api_repository api_repo = new Api_repository();
     private LiveData<List<BusinessNews>> news;
    
-    public NewsFeed_ViewModel() { 
-		//articles = new MutableLiveData<>();
-        //news = api_repository.getArticleContent();
-    }
-	/*
-    private MutableLiveData<List<BusinessNews>> articles;
-    private api_service mRestful_API;
-    private Call<List<BusinessNews>> mRequestCall;
-
-    public NewsFeed_ViewModel() {
-        mRestful_API = RetrofitClient.getRetrofitInstance().create(api_service.class);
-        mRequestCall = mRestful_API.get_article_content();
-        articles = new MutableLiveData<>();
-    }*/
-
-    /**
-     * @return We use MutableLiveData because we want to access the live data by assigning it the response we get
-     * from the RESTful API
-     * Mutable means "to change", LiveData by default has no modifiers that allow it to be changed but using the
-     * keyword Mutable will do so.
-     
-    public void getArticleContent() {
-        mRequestCall.enqueue(new Callback<List<BusinessNews>>() {
-            @Override
-            public void onResponse(Call<List<BusinessNews>> call, Response<List<BusinessNews>> response) {
-                if(response.isSuccessful()) {
-                    articles.setValue(response.body());
-                    //Think of it as passing the response body "out of this async code block" and treat it as synchronous code
-                }
-            }
-            @Override
-            public void onFailure(Call<List<BusinessNews>> call, Throwable t) {
-                articles.setValue(null);
-            }
-        });
-        //return articles;
-    }*/
-    
     public void getNews(){
 	api_repo.getArticleContent();
     }
 
     public LiveData<List<BusinessNews>> getBusinessNewsItems() {
-        //return news;
         return api_repo.getArticles();
     }
 }
