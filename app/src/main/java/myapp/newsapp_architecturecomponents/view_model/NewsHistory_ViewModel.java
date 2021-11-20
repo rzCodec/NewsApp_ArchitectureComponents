cdcd5af961001ac83a2c4194f188e45207bc0d0a
Ronald <laironald52@gmail.com>
2021-11-21 00:00:08 +0200

    Update NewsHistory_ViewModel.java

--- app/src/main/java/myapp/newsapp_architecturecomponents/view_model/NewsHistory_ViewModel.java
+++ app/src/main/java/myapp/newsapp_architecturecomponents/view_model/NewsHistory_ViewModel.java
@@ -6,21 +6,15 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import myapp.newsapp_architecturecomponents.repository.BusinessNews;
 import myapp.newsapp_architecturecomponents.repository.database_repository.NewsDatabase_Repository;
-
 import java.util.List;
 
-/**
- * Class for the UI that the user can see their saved news
- */
 public class NewsHistory_ViewModel extends AndroidViewModel {
-
-    private LiveData<List<BusinessNews>> database_newsItems; //LiveData is used so whatever changes are made are reflected
+    private LiveData<List<BusinessNews>> database_newsItems;
     private NewsDatabase_Repository newsDatabase_repository;
 
     public NewsHistory_ViewModel(@NonNull Application application) {
         super(application);
         newsDatabase_repository = new NewsDatabase_Repository(application);
-	//Retrieve the news from the db repo and get it ready to be returned and displayed in a RecyclerView and CardView
         this.database_newsItems = newsDatabase_repository.getDatabase_newsItems();
     }
 
